b23a95bcb8c4de84e344396e48c5e8fa4ce9f1c7
kimestelle <137699667+kimestelle@users.noreply.github.com>
2024-06-22 02:38:16 -0400

    added scroll intuition

--- src/components/slides/Cover.jsx
+++ src/components/slides/Cover.jsx
@@ -1,9 +1,13 @@
 import './Cover.css'
 import newspapers from '../../assets/newspapers.png'
+import scroll from '../../assets/scroll.svg'
 
 const Cover = () => {
     return (
         <div className='cover'>
+            <div className='scroll-container'>
+                <img src={scroll} className='scroll'/>
+            </div>
             <img src={newspapers}/>
         </div>
     )
